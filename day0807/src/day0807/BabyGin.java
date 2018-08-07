87722879ef58ae75bb9f91a1c77045accaa42417
ojeongeuni <zyzm12@gmail.com>
2018-08-07 17:17:11 +0900

    baby gin- 탐욕

---
+++ day0807/src/day0807/BabyGin.java
@@ -0,0 +1,87 @@
+package day0807;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BabyGin {
+	/*
+	run : 연속된 숫자 3개
+	triplet : 동일한 숫자 3개
+	baby-gin : run, triplet으로 구성된 문자열
+	*/
+	static int counts[];//0~9까지의 숫자의 빈도수 저장 
+						//각원소를 체크하며 run, triplet, baby-gin 여부 판단
+	
+	static boolean tri;
+	static boolean r;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String s = br.readLine();
+		
+		counts = new int[10]; //0,1,2,3, .. ~ 9
+		
+		for(int i=0; i<s.length(); i++) {
+			int index = s.charAt(i)-'0';
+			counts[index]++;
+		}
+		
+		tri=false;
+		r=false;
+		triplet(counts);
+		run(counts);
+		
+		if(tri==true && r==true) {
+			System.out.println("Baby-Gin");
+			
+		}else if(tri==true && r==false){
+			System.out.println("triplet, but No Baby-Gin");
+			
+		}else if(tri==false && r==true){
+			System.out.println("run, but No Baby-Gin");
+		}
+		
+	}
+	
+	public static void triplet(int counts[]) {
+		for(int i=0; i<counts.length; i++) {
+			//counts[i] 3이상인것 찾으면
+			if(counts[i]>=3) {
+				//3차감해주고
+				counts[i] -= 3;
+				//triplet 표시
+				tri = true;
+			}
+		}
+	}
+	
+	public static void run(int counts[]) {
+		for(int i=0; i<counts.length ;i++) {
+			//counts[i] 1이상인것 찾으면
+			if(counts[i]>=1) {
+				boolean flag=false;
+				
+				//counts[i], counts[i+1], counts[i+2] 1이상인지 조사
+				for(int j=0; j<3; j++) {
+					//중간에 1미만인것 발견되면 break
+					if(counts[i+j]<1) {
+						flag=false;
+						break;
+					}
+					
+					flag=true;
+				}
+				
+				//counts[i], counts[i+1], counts[i+2] 모두 1이상이면
+				if(flag==true) {
+					//1씩 차감해주고, run표시
+					for(int k=0;k<3; k++) {
+						counts[i+k]-=1;
+						r=true;
+					}
+				}
+			}
+		}
+	}
+
+}
--- day0807/src/day0807/SW_1248.java
+++ day0807/src/day0807/SW_1248.java
@@ -1,7 +1,90 @@
 package day0807;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class SW_1248 {
-	public static void main(String[] args) {
+	static int T;
+	static int V;
+	static int E;
+	static int A;
+	static int B;
+	static int tree[][];
+	static int visited[];
+	static String result1;
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		T = Integer.parseInt(br.readLine());
+		for(int t=1; t<=T; t++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			while(st.hasMoreTokens()) {
+				V = Integer.parseInt(st.nextToken());//정점의 총 수
+				E = Integer.parseInt(st.nextToken());//간선의 총 수
+				
+				//자식들
+				A = Integer.parseInt(st.nextToken());
+				B = Integer.parseInt(st.nextToken());
+			
+			}
+			
+			tree = new int[V+1][V+1];
+			visited = new int[V+1];
+			st = new StringTokenizer(br.readLine());
+			while(st.hasMoreTokens()) {
+				int p = Integer.parseInt(st.nextToken());
+				int c = Integer.parseInt(st.nextToken());
+				
+				tree[p][c]=1;
+				
+			}
+			
+			int cnt=0;
+			
+			for(int i=0; i<V+1; i++) {
+				for(int j=0; j<V+1; j++) {
+					if(tree[i][j]==1) {
+						cnt++;
+					}
+					if(cnt==2) {
+						result1="";
+						System.out.println(i);
+						dfs(i);
+					}
+				}
+			}
+			//dfs(1);
+			
+			
+			System.out.println("#"+t);
+			System.out.println(result1);
+/*			for(int i=0; i<V+1; i++) {
+				for(int j=0; j<V+1; j++) {
+					System.out.print(tree[i][j]);
+				}
+				System.out.println();
+			}*/
+			
+			
+			
+		}
+		
+		
 		
 	}
+	
+	public static void dfs(int v) {
+		visited[v]=1;
+		result1 += String.valueOf(v)+" ";
+		
+		for(int j=0; j<V+1; j++) {
+			if(tree[v][j]==1 && visited[j]==0) {
+				dfs(j);
+			}
+		}
+		
+	}
+	
+	
 }
