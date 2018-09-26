6f79a266c915a6569e5ffb1e374e779159d545fc
ojeongeuni <zyzm12@gmail.com>
2018-09-27 03:19:20 +0900

    이게 dp..?

--- day0925/src/day0925/Solution2.java
+++ day0925/src/day0925/Solution2.java
@@ -6,16 +6,18 @@ import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.StringTokenizer;
  
-public class Solution {
+public class Solution2 {
     static int T;
     static int n;
     static int arr[][];
     static int visited[][];
+    static boolean flag;
+    static int t;
     public static void main(String[] args) throws NumberFormatException, IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         T = Integer.parseInt(br.readLine());
          
-        for(int t=1; t<=T; t++) {
+        for(t=1; t<=T; t++) {
             n = Integer.parseInt(br.readLine());
             arr = new int[n][2];
             visited = new int[n][2];
@@ -27,28 +29,36 @@ public class Solution {
             }
              
             LinkedList<Integer> list = new LinkedList<>();
-
+             
             list.add(arr[0][0]);
             list.add(arr[0][1]);
+             
+            flag=false;
             dfs(0, list);
 
-            System.out.print("#"+t+" ");
-            for(int i=0; i<list.size(); i++) {
-                System.out.print(list.get(i)+" ");
-            }
-            System.out.println();
         }
          
     }
      
     public static void dfs(int row, LinkedList<Integer> list) {
-         
+        if(flag==true) return;
         int a = list.peekFirst(); //리스트 숫
         int b = list.peekLast(); //리스트 암
         visited[row][0]=1;
+ 
+        //System.out.println(list); 
+        
+        if(list.size()==2*n) {
+            flag = true;
+             
+            System.out.print("#"+t+" ");
+            for(int i=0; i<list.size(); i++) {
+                System.out.print(list.get(i)+" ");
+            }
+            System.out.println();
+            return;
+        }
          
-        //System.out.println(a+" "+b);
-        System.out.println(list);
          
             for(int i=0; i<n; i++) {
                 int na = arr[i][0]; //new 숫
@@ -61,17 +71,30 @@ public class Solution {
                     list.addFirst(nb);
                     list.addFirst(na);
                      
+                    dfs(i, list);
+                    list.removeFirst();
+                    list.removeFirst();
+                    visited[i][0]=0;
+                     
                 //리스트 맨 뒷 암놈과 새로운 숫놈 같으면
                 }else if(b==na) {
                     list.addLast(na);
                     list.addLast(nb);
                  
-                }else {
+                    dfs(i, list);
+                    list.removeLast();
+                    list.removeLast();
+                    visited[i][0]=0;
+                     
+                }/*else {
                     continue;
-                }
+                }*/
                      
                  
-                dfs(i, list);
+                /*dfs(i, list);
+                //visited[i][0]=0;
+*/         
+             
              
         }
          
