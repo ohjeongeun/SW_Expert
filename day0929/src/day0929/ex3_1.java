88d628cbb43cdd0c827e1dc9a8c9fb22204ea3b3
ojeongeuni <zyzm12@gmail.com>
2018-10-01 02:40:23 +0900

    .

--- day0929/src/day0929/ex3_1.java
+++ day0929/src/day0929/ex3_1.java
@@ -7,12 +7,14 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
-public class ex3 {
+public class ex3_1 {
 	static int map[][];
 	static int visited[];
 	static int max;
+	static Queue<Integer> q;
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
@@ -21,25 +23,31 @@ public class ex3 {
 		map = new int[1001][1001];
 		visited = new int[1001];
 		
+		int maxValue=0;
 		LinkedList<Integer> start = new LinkedList<>();
 		for(int i=0; i<N; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			ArrayList<Integer> list = new ArrayList<>();
 			int s = Integer.parseInt(st.nextToken());
+			
 			start.add(s);
 			list.add(s);
 			while(st.hasMoreTokens()) {
-				list.add(Integer.parseInt(st.nextToken()));
+				int t = Integer.parseInt(st.nextToken());
+				list.add(t);
+				if(maxValue<t) {
+					maxValue=t;
+				}
 			}
 			
-			//System.out.println(list);
 			for(int j=0; j<list.size()-1; j++) {
 				map[list.get(j)][list.get(j+1)]=1;
 			}
 			
 		}
 		
-		
+		System.out.println("*"+maxValue);
+		//Ãâ·Â
 		for(int i=0; i<21; i++) {
 			for(int j=0; j<21; j++) {
 				System.out.print(map[i][j]);
@@ -47,15 +55,27 @@ public class ex3 {
 			System.out.println();
 		}
 		
+		q = new LinkedList<>();
 		ArrayList<Integer> result = new ArrayList<>();
-		for(int i=0; i<N; i++) {
+		
+		while(!start.isEmpty()) {
+			max=0;
+			bfs(start.remove());
+			if(!result.contains(max)) {
+				result.add(max);
+			}
+		}
+		max=0;
+		bfs(13);
+		
+		/*for(int i=0; i<N; i++) {
 			max=0;
 			dfs(start.remove());
 			if(!result.contains(max)) {
 				result.add(max);
 			}
 			
-		}
+		}*/
 		
 		Collections.sort(result);
 		//System.out.println(result);
@@ -63,22 +83,40 @@ public class ex3 {
 			System.out.print(result.get(i)+" ");
 		}
 		
+		
+		
 	}
 	
-	public static void dfs(int v) {
-		if(max<v) {
-			max=v;
-		}
+	public static void bfs(int v) {
+		q.add(v);
+		visited[v]=1;
 		
-		int cnt=0;
-		for(int j=0; j<21; j++) {
-			if(map[v][j]==1 && visited[j]==0) {
-				visited[j]=1;
-				dfs(j);
-				visited[j]=0;
+		while(!q.isEmpty()) {
+			int cur =  q.remove();
+			if(max<cur) {
+				max=cur;
 			}
 			
+			int cnt=0;
+			for(int j=0; j<1001; j++) {
+				if(map[cur][j]==1 && visited[j]==0) {
+					cnt++;
+					if(cnt>1) {
+						System.out.println(-1);
+						return;
+					}
+					q.add(j);
+					visited[j]=1;
+				}
+				
+			}
+			
+			
+			
+			
 		}
+		
+		
 		
 	}
 	
